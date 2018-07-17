fc7657092a6c32ab903154679b1619131d8cc004
Taylor Gentry <taylor.gentry300@gmail.com>
2018-07-17 15:45:31 -0400

    files created

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(publicPath));
+app.use(express.json())
